be6ae350e2b97a108b062d69ac90a01d339f04b7
Siddharth Singh <me@shantaram.xyz>
2024-02-12 13:33:53 +0530

    change homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,10 @@ export default function HomePage() {
       <section className="py-16 md:py-24 lg:py-32">
         <div className="container flex max-w-screen-md flex-col items-center gap-4 text-center">
           <h1 className="!leading-none font-bold text-3xl sm:text-5xl md:text-6xl lg:text-7xl landing-header py-2">
-            Welcome, friends of shantaram.
+            Welcome, friends!.
           </h1>
           <p className="max-w-[42rem] leading-normal sm:text-xl sm:leading-8">
             This is spliit, a free and open-source Splitwise alternative. Go ahead and create a group for you and your friends!
-          </p>
-
-          <p className="max-w-[42rem] leading-normal sm:text-xl sm:leading-8">
-            Please be kind though, don&apos;t fill it with weird spam or vandalism.
           </p>
           <div className="flex gap-2">
             <Button asChild>
